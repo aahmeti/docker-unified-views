a2812802a75206f8f1fa507a8ad7e73a0aa27ae2
Tomas Knap <toknap@gmail.com>
2017-09-19 12:04:42 +0200

    To reflect changes in module.path

--- uv-shared/config/start.sh
+++ uv-shared/config/start.sh
@@ -9,9 +9,10 @@ cp /config/*.properties /unified-views/config/
 ln -s /unified-views/config/config.properties /etc/unifiedviews/config/config.properties 
 
 mkdir -p /unified-views/logs
-mkdir -p /unified-views/dpu
-mkdir -p /unified-views/lib
+mkdir -p /unified-views/dpus/dpu
+mkdir -p /unified-views/dpus/lib
 mkdir -p /unified-views/backend
+mkdir -p /unified-views/lib
 mkdir -p /unified-views/backend/working/temp/export 
 mkdir -p /unified-views/backend/working/temp/import
 mkdir -p /unified-views/feeds
